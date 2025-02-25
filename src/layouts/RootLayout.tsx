7594117e9164447497bb1c5d6eb49aef4824e0c2
Rosintan Sitinjak <2381014@unai.edu>
2025-02-25 20:12:54 +0700

    push handson-6

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,13 +1,13 @@
-import { Outlet } from 'react-router-dom'
-import Navbar from '../components/Navbar'
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 const RootLayout = () => {
   return (
     <div>
-      <Navbar/>
-      <Outlet/>
+      <Navbar />
+      <Outlet />
     </div>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
